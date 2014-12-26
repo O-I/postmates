c5d873de1eb3c0dd032b305818802c1a47d06dcc
Rahul Horé <hore.rahul@gmail.com>
2014-12-25 21:36:53 -0500

    Adds vanilla postmates.rb

--- lib/postmates.rb
+++ lib/postmates.rb
@@ -1,5 +1,19 @@
-require "postmates/version"
+require_relative 'postmates/client'
 
 module Postmates
-  # Your code goes here...
+  class << self
+
+    def new
+      @client ||= Postmates::Client.new
+    end
+
+    def method_missing(method, *args, &block)
+      return super unless new.respond_to?(method)
+      new.send(method, *args, &block)
+    end
+
+    def respond_to?(method, include_private = false)
+      new.respond_to?(method, include_private) || super
+    end
+  end
 end
