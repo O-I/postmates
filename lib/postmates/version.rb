f47264443cfc4673bee91d9909b2271fc3c87dee
Rahul Horé <hore.rahul@gmail.com>
2014-12-25 21:31:31 -0500

    Adds vanilla version.rb

--- lib/postmates/version.rb
+++ lib/postmates/version.rb
@@ -1,3 +1,14 @@
 module Postmates
-  VERSION = "0.0.1"
+  class Version
+    MAJOR = 0
+    MINOR = 0
+    PATCH = 1
+    PRE   = nil
+
+    class << self
+      def to_s
+        [MAJOR, MINOR, PATCH, PRE].compact.join('.')
+      end
+    end
+  end
 end
