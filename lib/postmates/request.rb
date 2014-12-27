b081d95da1dedb939b9fa4621df409918ed33394
Rahul Horé <hore.rahul@gmail.com>
2014-12-26 20:48:03 -0500

    Adds vanilla request.rb

---
+++ lib/postmates/request.rb
@@ -0,0 +1,26 @@
+module Postmates
+  module Request
+    def get(path, options = {})
+      request(:get, path, options)
+    end
+
+    def post(path, options = {})
+      request(:post, path, options)
+    end
+
+    private
+
+    def request(method, path, options)
+      response = connection.send(method) do |request|
+        case method
+        when :get 
+          request.url(path, options)
+        when :post
+          request.path = path
+          request.body = options unless options.empty?
+        end
+      end
+      response.body
+    end
+  end
+end
