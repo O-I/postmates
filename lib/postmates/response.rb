2f0532bbf44911321e1a323147663bd37ac69b7d
Rahul Horé <hore.rahul@gmail.com>
2015-01-03 20:58:42 -0500

    Adds meta attributes to list responses
    When a list of objects is returned, `total_count` and `next_href` attributes are returned with the response to support pagination for resources that are too large for a single query or have been explicitly partitioned with a limit parameter. These attributes are dynamically added to the returned Array object so they can be easily accessed.

--- lib/postmates/response.rb
+++ lib/postmates/response.rb
@@ -7,9 +7,16 @@ module Postmates
       def build(body)
         kind = body['object'] || body['kind']
         case kind
-        when 'list'           ; body['data'].map { |del| Delivery.new(del) }
-        when 'delivery'       ; Delivery.new(body)
-        when 'delivery_quote' ; Quote.new(body)
+        when 'list'
+          body['data'].map { |del| Delivery.new(del) }.tap do |list|
+            list.instance_variable_set(:@total_count, body['total_count'])
+            list.instance_variable_set(:@next_href, body['next_href'])
+            list.class.module_eval { attr_reader :total_count, :next_href }
+          end
+        when 'delivery'
+          Delivery.new(body)
+        when 'delivery_quote'
+          Quote.new(body)
         end
       end
     end
