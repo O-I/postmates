7b7a4710af8467f8b80dbde1d43eb15cad3626e0
mattsprig <matt@sprig.com>
2017-05-22 12:07:42 -0700

    Handle customer_suspended and all other unknown errors

--- lib/faraday/raise_http_exception.rb
+++ lib/faraday/raise_http_exception.rb
@@ -7,15 +7,20 @@ module FaradayMiddleware
     def call(env)
       @app.call(env).on_complete do |response|
         response_hash = JSON.parse(response.body)
-        msg = "#{response[:status]} #{response_hash['message']}"
+        msg = "#{response[:status]} #{response_hash['code']}: #{response_hash['message']}"
 
         case response[:status]
         when 400 ; raise Postmates::BadRequest,          msg
         when 401 ; raise Postmates::Unauthorized,        msg
+        when 402 ; raise Postmates::CustomerSuspended,   msg
         when 403 ; raise Postmates::Forbidden,           msg
         when 404 ; raise Postmates::NotFound,            msg
         when 500 ; raise Postmates::InternalServerError, msg
         when 503 ; raise Postmates::ServiceUnavailable,  msg
+        end
+
+        if response[:status] >= 300
+          raise Postmates::Error, msg
         end
       end
     end
